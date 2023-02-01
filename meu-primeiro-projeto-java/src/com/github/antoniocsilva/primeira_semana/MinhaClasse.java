fbf60d791b20fbe47f15e31e6b65e8b28de20e5c
Antonio Silva <antonios.carlos@hotmail.com>
2023-02-01 06:20:36 -0300

    Atualizando

---
+++ meu-primeiro-projeto-java/src/com/github/antoniocsilva/primeira_semana/MinhaClasse.java
@@ -0,0 +1,16 @@
+package com.github.antoniocsilva.primeira_semana;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String primeiroNome = "Antonio";
+        String segundoNome = "Silva";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
